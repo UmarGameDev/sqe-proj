91c85fdb1cb8a41be14f40b2568584b23fd5dd27
UmarGameDev <141553749+UmarGameDev@users.noreply.github.com>
2025-12-09 00:25:54 +0500

    Remove Sentry SDK integration
    Removed Sentry SDK initialization from main.py.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
 
 from backend.router import router
-import sentry_sdk
-
-sentry_sdk.init(
-    dsn=os.getenv("https://9dc605491367c51c28e8d1acc814867d@o4510492429582336.ingest.us.sentry.io/4510493799809024"),
-    send_default_pii=True,
-)
 
 app = FastAPI()
 
